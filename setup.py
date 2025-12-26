90dbf918ca289d1853603d10c1df5b5c99ead455
Luka L <licina.luka@outlook.com>
2025-12-26 08:36:13 +0100

    README for PyPI

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup
 setup(
     name="jinjatomic",
     version="0.0.1",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     install_requires=[
         "edn-format",
         "jinja2",
