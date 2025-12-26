d1565ccee7f9011e35ffd05606de2dcb2d358c37
Luka L <licina.luka@outlook.com>
2025-12-26 08:27:05 +0100

    up

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="jinjatomic",
+    version="0.0.1",
+    install_requires=[
+        "edn-format",
+        "jinja2",
+        "requests",
+    ],
+)
